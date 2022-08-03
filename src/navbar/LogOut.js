be19279d11edeaf64a39d13ba4ed59a7b87e9981
LusGevorgyan <lusinegevorgyan928@gmail.com>
2022-08-03 16:11:25 +0400

    I changed logOut button /

--- src/navbar/LogOut.js
+++ src/navbar/LogOut.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LogOut = () => {
   return (
     <div className='text-[12px] sm:text-[18px] xl:text-[24px] cursor-pointer font-bold text-[#B47AEA]'>
-        <p> Logout </p>
+    <Link to='/'>
+      <p> Logout </p>
+    </Link>
     </div>
   )
 }
